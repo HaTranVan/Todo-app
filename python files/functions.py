93bc8e544db2ec22a4b2fd4be54412fb4e34875a
HaTranVan <hatranvan2003@gmail.com>
2023-07-17 08:49:51 +0700

    Implement Edit feature and some small features

--- python files/functions.py
+++ python files/functions.py
@@ -1,11 +1,14 @@
-def get_todos(filepath):
+FILEPATH = '../files/todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
     """read and get todo as a list"""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath=FILEPATH):
     """write todos to a file"""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
